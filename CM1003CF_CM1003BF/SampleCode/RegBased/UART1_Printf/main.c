629b512cab595ce983aae5048ff4315d951f5a29
NR10 WWang1 <WWang1@nuvoton.com>
2026-06-11 18:21:04 +0800

    Update CM1003CF_CM1003BF UART1_Printf main.c

--- CM1003CF_CM1003BF/SampleCode/RegBased/UART1_Printf/main.c
+++ CM1003CF_CM1003BF/SampleCode/RegBased/UART1_Printf/main.c
@@ -17,6 +17,8 @@ void main (void)
 {
 /* Modify HIRC to 24MHz for UART baud rate deviation not over 1%*/
     MODIFY_HIRC(HIRC_24);
+    P16_QUASI_MODE;
+    P02_INPUT_MODE;
     UART_Open(24000000,UART1_Timer3,115200);
 
     while(1)
