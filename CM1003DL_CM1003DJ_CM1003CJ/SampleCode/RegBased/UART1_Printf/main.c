36a2cd5a3191cbc609c26242f3b35db5e4db6567
NR10 WWang1 <WWang1@nuvoton.com>
2026-06-12 09:17:47 +0800

    Update UART1_Printf main.c

--- CM1003DL_CM1003DJ_CM1003CJ/SampleCode/RegBased/UART1_Printf/main.c
+++ CM1003DL_CM1003DJ_CM1003CJ/SampleCode/RegBased/UART1_Printf/main.c
@@ -6,6 +6,7 @@
 /*---------------------------------------------------------------------------------------------------------*/
 #include "numicro_8051.h"
 
+#define     UART1_P16_P02
 
 /**
  * @brief       Main loop
@@ -17,6 +18,24 @@ void main (void)
 {
 /* Modify HIRC to 24MHz for UART baud rate deviation not over 1%*/
     MODIFY_HIRC(HIRC_24);
+	
+#ifdef   UART1_P16_P02            /*  if define UART1_P16_P02 initial GPIO and MFP */
+    P16_QUASI_MODE;
+    P02_INPUT_MODE;
+    ENABLE_UART1_TXD_P16;
+    ENABLE_UART1_RXD_P02;
+#elif defined  UART1_P36_P37      /*  if define UART1_P36_P37 initial GPIO and MFP */
+    P36_QUASI_MODE;
+    P37_INPUT_MODE;
+    ENABLE_UART1_TXD_P36;
+    ENABLE_UART1_RXD_P37;
+#elif defined  UART1_P10_P00      /*  if define UART1_P10_P00 initial GPIO and MFP */
+    P10_QUASI_MODE;
+    P00_INPUT_MODE;
+    ENABLE_UART1_TXD_P10;
+    ENABLE_UART1_RXD_P00;
+#endif
+
     UART_Open(24000000,UART1_Timer3,115200);
 
     while(1)
